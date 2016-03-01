1563755166579d0e876571d182e6f0c246feb268
Maggie Smith <mosmith2@illinois.edu>
2016-03-01 01:07:13 +0000

    navbar almost done and header image changed

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,12 @@
-// Write any custom javascript functions here
+$(window).scroll(function() {
+
+	var $titlebar = $('.title-bar');
+    var windscroll = $(window).scrollTop(); // shrink navbar after scrolling down
+    if (windscroll >= 400) {
+      $titlebar.addClass('navsmaller');
+    }
+    else{
+        $titlebar.removeClass('navsmaller');
+    } 
+
+}).scroll();
